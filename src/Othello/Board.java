8ebfb7e8b6a29a23b81c7318c5df4f9fc28cf8f2
FRANZEA17 <FRANZEA17@5CG73106NB.GCC.edu>
2018-11-26 23:32:04 -0500

    Kept working on methods

--- src/Othello/Board.java
+++ src/Othello/Board.java
@@ -2,15 +2,7 @@ package Othello;
 
 public class Board {
 	
-	
-	public Board() {
-		
-	}
-
-	
 	public static char[][] board = new char[10][10];
-	
-	
 	
 	/**
 	 * Creates the beginning board
@@ -29,8 +21,6 @@ public class Board {
 		board[5][4] = 'X';
 		board[5][5] = 'O';
 	}
-
-
 
 
 	/**
@@ -54,7 +44,6 @@ public class Board {
     }
     
     
-    
     /**
      * Determines if the board is full
      * @return a boolean - true if the board is full and false if the board is empty
@@ -76,7 +65,7 @@ public class Board {
     
     /**
      *Changes location to row
-     * @param location
+     * @param location an int that is the number of the space
      * @return an integer that represents the row of the location
      */
 	int getRow(int location) {
@@ -85,7 +74,7 @@ public class Board {
 	
 	/**
 	 * Changes location to column
-	 * @param location
+	 * @param location an int that is the number of the space
 	 * @return an integer that represents the column of the location
 	 */
 	public int getColumn(int location) {
@@ -102,11 +91,11 @@ public class Board {
     	 board[getRow(location)][getColumn(location)] = currentPlayer;
      }
      
+     
      /**
       * checks if a player move is valid
       * @param currentPlayer - the player that made the move
       * @param location - the space they made the move in 
-      * 
       */
      public boolean validPlay(char currentPlayer, int location) {
     	 boolean isValid = true;
@@ -127,8 +116,8 @@ public class Board {
      public int countPieces(char currentPlayer) {
     	 int holder = 0;
     	 
-    	 for (int i = 0; i < 8; i++) {
-             for (int j = 0; j < 8; j++) {
+    	 for (int i = 1; i < 9; i++) {
+             for (int j = 1; j < 9; j++) {
             	 if (board[i][j] == currentPlayer) {
             		 holder++;
             	 }
@@ -145,8 +134,100 @@ public class Board {
       * @location - the space they placed the piece
       */
      public void flip(char currentPlayer, int location) {
+    	 int r = getRow(location);
+    	 int c = getColumn(location);
+    	 //check row to the left
+    	 boolean flipLeftRow = false;
+    	 int toLeftColumn = c;
+    	 for(int i=c-1; i >= 0; i--) {
+    		 if (board[r][i] == ' ') {
+    			 break;
+    		 }
+    		 else if (board[r][i] == currentPlayer) {
+    			 toLeftColumn = i;
+    			 flipLeftRow = true;
+    			 break;
+    		 }
+    	 }
+    	 
+    	 //flip row to the left if applicable
+    	 if (flipLeftRow) {
+    		 for (int i=c-1; i >= toLeftColumn; i--) {
+    			 board[r][i] = currentPlayer;
+    		 }
+    	 }
+    	
     	 
     	 
+    	//check row to the right
+    	 boolean flipRightRow = false;
+    	 int toRightColumn = c;
+    	 for(int i=c+1; i < 10; i++) {
+    		 if (board[r][i] == ' ') {
+    			 break;
+    		 }
+    		 else if (board[r][i] == currentPlayer) {
+    			 toRightColumn = i;
+    			 flipRightRow = true;
+    			 break;
+    		 }
+    	 }
+    	 
+    	 //flip row to the right if applicable
+    	 if (flipRightRow) {
+    		 for (int i=c+1; i <= toRightColumn; i++) {
+    			 board[r][i] = currentPlayer;
+    		 }
+    	 }
+    	 
+    	 
+    	 
+    	 //check column above
+    	 boolean flipColumnAbove = false;
+    	 int toRowAbove = r;
+    	 for (int i=r-1; r > 0; r--) {
+    		 if (board[i][c] == ' ') {
+    			 break;
+    		 }
+    		 else if (board[i][c] == currentPlayer) {
+    			 toRowAbove = i;
+    			 flipColumnAbove = true;
+    			 break;
+    		 }
+    	 }
+    	 
+    	 //flip column above if applicable
+    	 if (flipColumnAbove) {
+    		 for (int i=r-1; r >= toRowAbove; r--) {
+    			 board[i][c] = currentPlayer;
+    		 }
+    	 }
+    	 
+    	 
+    	 
+    	//check column below
+    	 boolean flipColumnBelow = false;
+    	 int toRowBelow = r;
+    	 for (int i=r+1; r > 10; r++) {
+    		 if (board[i][c] == ' ') {
+    			 break;
+    		 }
+    		 else if (board[i][c] == currentPlayer) {
+    			 toRowBelow = i;
+    			 flipColumnBelow = true;
+    			 break;
+    		 }
+    	 }
+    	 
+    	 //flip column below if applicable
+    	 if (flipColumnBelow) {
+    		 for (int i=r+1; r <= toRowBelow; r++) {
+    			 board[i][c] = currentPlayer;
+    		 }
+    	 }
+    	 
+    	 
+    	 //TODO all diagonals
      }
      
      
