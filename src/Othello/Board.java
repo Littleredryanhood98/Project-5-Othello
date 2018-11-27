8322ad973b80c4a24b323e253dbee197ef1ba587
FRANZEA17 <FRANZEA17@5CG73106NB.GCC.edu>
2018-11-26 21:42:22 -0500

    added validPlay and began filling in methods

--- src/Othello/Board.java
+++ src/Othello/Board.java
@@ -57,7 +57,7 @@ public class Board {
     
     /**
      * Determines if the board is full
-     * @return
+     * @return a boolean - true if the board is full and false if the board is empty
      */
     public boolean isFull() {
         boolean isFull = true;
@@ -77,48 +77,72 @@ public class Board {
     /**
      *Changes location to row
      * @param location
-     * @return
+     * @return an integer that represents the row of the location
      */
 	int getRow(int location) {
-		
 		return (Math.floorDiv(location-1, 8));
 	}
 	
 	/**
 	 * Changes location to column
 	 * @param location
-	 * @return
+	 * @return an integer that represents the column of the location
 	 */
 	int getColumn(int location) {
-		
 		return 0;
 	}
     
     
     /**
-     * Fills board with players moves while making sure they are legal.
-     * @param currentPlayer
-     * @param location
-     * @throws Exception
+     * fills board with player moves 
+     * @param currentPlayer - the player who made the move
+     * @param location - the location that the player just placed a piece
      */
-     void play(char currentPlayer, int location) throws Exception{
-     	
+     public void play(char currentPlayer, int location) {
+    
      }
      
+     /**
+      * checks if a player move is valid
+      * @param currentPlayer - the player that made the move
+      * @param location - the space they made the move in 
+      * 
+      */
+     public boolean validPlay(char currentPlayer, int location) {
+    	 boolean isValid = true;
+    	 
+    	 if (board[getRow(location)][getColumn(location)] == ' ']) {
+    		isValid = true;
+    	 }
+    	 
+    	 
+     }
      
      
      /**
       * Counts the number of pieces on the board for both players
+      * @param currentPlayer the player that you are counting the pieces of
+      * @return the number of pieces that player has on the board
       */
-     void count() {
+     public int countPieces(char currentPlayer) {
+    	 int holder = 0;
     	 
+    	 for (int i = 0; i < 8; i++) {
+             for (int j = 0; j < 8; j++) {
+            	 if (board[i][j] == currentPlayer) {
+            		 holder++;
+            	 }
+             }
+    	 }
+    	 
+    	 return holder;
      }
      
      
      /**
       * Flips adjacent tiles to the one just placed
       */
-     void flip() {
+     public void flip() {
     	 
      }
      
