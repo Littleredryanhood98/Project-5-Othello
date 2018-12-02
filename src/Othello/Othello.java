8bc8326722883d3d010fbd5d428e874c08ce07be
YOUNGJT17 <YOUNGJT17@5CG73103VN.GCC.edu>
2018-12-02 14:41:58 -0500

    supposedly working Othello

--- src/Othello/Othello.java
+++ src/Othello/Othello.java
@@ -1,5 +1,7 @@
 package Othello;
 
+import java.util.Scanner;
+
 public class Othello {
 	
 //	Create Game and call gameloop
@@ -9,9 +11,27 @@ public class Othello {
 	
 	
 	public static void main(String[] args) {
-		
-		Game Othello = new Game();
+	String plrname;
+	String plrname2;
+	boolean Ai;
+	int numplr;
+	Scanner scrn = new Scanner(System.in);
+		System.out.println("Enter the number of players no more than two players:");
+		numplr = scrn.nextInt();
+		if (numplr == 2) {
+			System.out.println("Player one enter your name:");
+			plrname = scrn.next();
+			System.out.println("Player two enter your name:");
+			plrname2 = scrn.next();
+			Game Othello = new Game(plrname,false,plrname2,false);
 		Othello.gameLoop();
+		} else if(numplr == 1) {
+			System.out.println("Player one enter your name:");
+			plrname = scrn.next();
+			Game Othello = new Game(plrname,false,"Computer",true);
+		Othello.gameLoop();
+		}
+		
 	
 //	
 //	Board b = new Board();
